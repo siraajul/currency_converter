6a45967ac6371c59e04851d98d1f955d21e1b1bd
siraajul <shirajulislamparvez@gmail.com>
2024-10-30 05:26:45 +0600

    Working: TextField

--- lib/screens/home_page_material.dart
+++ lib/screens/home_page_material.dart
@@ -5,6 +5,18 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    const border = OutlineInputBorder(
+      borderSide: BorderSide(
+        color: Colors.black,
+        width: 2,
+        style: BorderStyle.solid,
+        strokeAlign: BorderSide.strokeAlignInside,
+      ),
+      borderRadius: BorderRadius.all(
+        Radius.circular(40),
+      ),
+    );
+//---------------------------------------Page Entry-----------------------------
     return Scaffold(
       backgroundColor: Colors.blueGrey,
       body: Center(
@@ -14,9 +26,29 @@ class HomePage extends StatelessWidget {
             Text(
               '0',
               style: TextStyle(
-                fontSize: 45,
-                fontWeight: FontWeight.bold,
-                color: Colors.white
+                  fontSize: 45,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white),
+            ),
+            TextField(
+              style: TextStyle(
+                color: Colors.black,
+              ),
+              keyboardType:
+                  TextInputType.numberWithOptions(decimal: true, signed: true),
+
+              decoration: InputDecoration(
+                hintText: 'Enter Your Amount in USD',
+                hintStyle: TextStyle(color: Colors.black),
+                prefixIcon: Icon(
+                  Icons.monetization_on_outlined,
+                  color: Colors.black,
+                  size: 25,
+                ),
+                filled: true,
+                fillColor: Colors.white,
+                focusedBorder: border,
+                enabledBorder: border,
               ),
             ),
           ],
