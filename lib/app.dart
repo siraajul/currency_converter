b4b650b3b2feb59aa0f5a88bf0a76d731c540dcc
siraajul <shirajulislamparvez@gmail.com>
2024-10-29 19:41:56 +0600

    Add: HomePage, Update: debugBanner

--- lib/app.dart
+++ lib/app.dart
@@ -1,3 +1,4 @@
+import 'package:currency_converter/screens/home_page_material.dart';
 import 'package:flutter/material.dart';
 
 class MyApp extends StatelessWidget {
@@ -5,8 +6,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-
+    return const MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: HomePage(),
     );
   }
 }
