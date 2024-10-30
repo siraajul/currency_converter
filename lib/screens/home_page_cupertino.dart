c31169003a793fc6921e134c5fbc518ed927b682
siraajul <shirajulislamparvez@gmail.com>
2024-10-31 03:07:35 +0600

    Add: Cupertino App

--- lib/screens/home_page_cupertino.dart
+++ lib/screens/home_page_cupertino.dart
@@ -1,10 +1,101 @@
 import 'package:flutter/cupertino.dart';
 
-class HomePageCupertino extends StatelessWidget {
-  const HomePageCupertino({super.key});
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _CurrencyConverterMaterialPageState();
+}
+
+class _CurrencyConverterMaterialPageState extends State<HomePage> {
+  double result = 0;
+  final TextEditingController textEditingController = TextEditingController();
+
+  void convert() {
+    result = double.parse(textEditingController.text) * 114;
+    setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    const border = OutlineInputBorder(
+      borderSide: BorderSide(
+        color: Colors.black,
+        width: 2,
+        style: BorderStyle.solid,
+        strokeAlign: BorderSide.strokeAlignInside,
+      ),
+      borderRadius: BorderRadius.all(
+        Radius.circular(10),
+      ),
+    );
+//---------------------------------------Page Entry-----------------------------
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Currency Converter',
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: Colors.blue,
+        elevation: 0,
+        centerTitle: true,
+      ),
+      backgroundColor: Colors.blue,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              maxLines: 1,
+              '৳${result != 0 ? result.toStringAsFixed(2) : result.toStringAsFixed(0)}',
+              style: const TextStyle(
+                  fontSize: 45,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10),
+              child: TextField(
+                controller: textEditingController,
+                style: const TextStyle(
+                  color: Colors.black,
+                ),
+                keyboardType: const TextInputType.numberWithOptions(
+                    decimal: true, signed: true),
+                decoration: const InputDecoration(
+                  hintText: 'Enter Your Amount in USD',
+                  hintStyle: TextStyle(color: Colors.black),
+                  prefixIcon: Icon(
+                    Icons.monetization_on_outlined,
+                    color: Colors.black,
+                    size: 25,
+                  ),
+                  filled: true,
+                  fillColor: Colors.white,
+                  focusedBorder: border,
+                  enabledBorder: border,
+                ),
+              ),
+            ),
+            //Button------------------------------------------------------------
+            Padding(
+              padding: const EdgeInsets.all(10),
+              child: TextButton(
+                onPressed: convert,
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.black,
+                  foregroundColor: Colors.white,
+                  minimumSize: const Size(double.infinity, 50),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+                child: const Text('Convert'),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
