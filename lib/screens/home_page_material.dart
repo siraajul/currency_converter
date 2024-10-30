d82b67a00639dad12772c3b47b7e796c04a10c21
siraajul <shirajulislamparvez@gmail.com>
2024-10-31 02:49:15 +0600

    Mod: Stateless --> Stateful

--- lib/screens/home_page_material.dart
+++ lib/screens/home_page_material.dart
@@ -1,16 +1,19 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
-  //1. Create a Value that stores the converted currency value
-  //2. Create a function that multiplies the value give by the textfield with BDT
-  //3. Store the value in the variable that we created
-  //4. Display the variable
+  @override
+  State<HomePage> createState() => _CurrencyConverterMaterialPageState();
+}
+
+class _CurrencyConverterMaterialPageState extends State<HomePage> {
+  double result = 0;
+  final TextEditingController textEditingController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
+    print('ReBuild');
     const border = OutlineInputBorder(
       borderSide: BorderSide(
         color: Colors.black,
@@ -25,32 +28,36 @@ class HomePage extends StatelessWidget {
 //---------------------------------------Page Entry-----------------------------
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Currency Converter',style: TextStyle(color: Colors.white),),
-        backgroundColor: Colors.blueGrey,
+        title: const Text(
+          'Currency Converter',
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: Colors.blue,
         elevation: 0,
         centerTitle: true,
       ),
-      backgroundColor: Colors.blueGrey,
+      backgroundColor: Colors.blue,
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Text(
-              '0',
-              style: TextStyle(
+            Text(
+             '৳$result',
+              style: const TextStyle(
                   fontSize: 45,
                   fontWeight: FontWeight.bold,
                   color: Colors.white),
             ),
-            const Padding(
-              padding: EdgeInsets.all(10),
+            Padding(
+              padding: const EdgeInsets.all(10),
               child: TextField(
-                style: TextStyle(
+                controller: textEditingController,
+                style: const TextStyle(
                   color: Colors.black,
                 ),
-                keyboardType: TextInputType.numberWithOptions(
+                keyboardType: const TextInputType.numberWithOptions(
                     decimal: true, signed: true),
-                decoration: InputDecoration(
+                decoration: const InputDecoration(
                   hintText: 'Enter Your Amount in USD',
                   hintStyle: TextStyle(color: Colors.black),
                   prefixIcon: Icon(
@@ -65,17 +72,14 @@ class HomePage extends StatelessWidget {
                 ),
               ),
             ),
-            //Button
+            //Button------------------------------------------------------------
             Padding(
               padding: const EdgeInsets.all(10),
               child: TextButton(
                 onPressed: () {
-                  //Debug--Release, Profile
-
-                  // print('Button Clicked');
-                  if (kDebugMode) {
-                    print('Button Clicked');
-                  }
+                  setState(() {
+                    result = double.parse(textEditingController.text) * 114;
+                  });
                 },
                 style: TextButton.styleFrom(
                   backgroundColor: Colors.black,
@@ -94,3 +98,107 @@ class HomePage extends StatelessWidget {
     );
   }
 }
+//-----------------------------------------------------------------------------
+// class HomePagee extends StatelessWidget {
+//   const HomePagee({super.key});
+//
+//   //--------To-Do--------------------
+//   //1. Create a Value that stores the converted currency value
+//   //2. Create a function that multiplies the value give by the textfield with BDT
+//   //3. Store the value in the variable that we created
+//   //4. Display the variable
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     double result = 0;
+//     final TextEditingController textEditingController = TextEditingController();
+//
+//     const border = OutlineInputBorder(
+//       borderSide: BorderSide(
+//         color: Colors.black,
+//         width: 2,
+//         style: BorderStyle.solid,
+//         strokeAlign: BorderSide.strokeAlignInside,
+//       ),
+//       borderRadius: BorderRadius.all(
+//         Radius.circular(10),
+//       ),
+//     );
+// //---------------------------------------Page Entry-----------------------------
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text(
+//           'Currency Converter',
+//           style: TextStyle(color: Colors.white),
+//         ),
+//         backgroundColor: Colors.blue,
+//         elevation: 0,
+//         centerTitle: true,
+//       ),
+//       backgroundColor: Colors.blue,
+//       body: Center(
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: [
+//             Text(
+//               result.toString(),
+//               style: const TextStyle(
+//                   fontSize: 45,
+//                   fontWeight: FontWeight.bold,
+//                   color: Colors.white),
+//             ),
+//             Padding(
+//               padding: const EdgeInsets.all(10),
+//               child: TextField(
+//                 controller: textEditingController,
+//                 style: const TextStyle(
+//                   color: Colors.black,
+//                 ),
+//                 keyboardType: const TextInputType.numberWithOptions(
+//                     decimal: true, signed: true),
+//                 decoration: const InputDecoration(
+//                   hintText: 'Enter Your Amount in USD',
+//                   hintStyle: TextStyle(color: Colors.black),
+//                   prefixIcon: Icon(
+//                     Icons.monetization_on_outlined,
+//                     color: Colors.black,
+//                     size: 25,
+//                   ),
+//                   filled: true,
+//                   fillColor: Colors.white,
+//                   focusedBorder: border,
+//                   enabledBorder: border,
+//                 ),
+//               ),
+//             ),
+//             //Button------------------------------------------------------------
+//             Padding(
+//               padding: const EdgeInsets.all(10),
+//               child: TextButton(
+//                 onPressed: () {
+//                   //Debug--Release, Profile
+//                   // print('Button Clicked');
+//                   // if (kDebugMode) {
+//                   //   print('Button Clicked');
+//                   // }
+//                   // print(textEditingController.text);
+//                   // print(double.parse(textEditingController.text) * 114);
+//                   result = double.parse(textEditingController.text) * 114;
+//                 },
+//                 style: TextButton.styleFrom(
+//                   backgroundColor: Colors.black,
+//                   foregroundColor: Colors.white,
+//                   minimumSize: const Size(double.infinity, 50),
+//                   shape: RoundedRectangleBorder(
+//                     borderRadius: BorderRadius.circular(10),
+//                   ),
+//                 ),
+//                 child: const Text('Convert'),
+//               ),
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
