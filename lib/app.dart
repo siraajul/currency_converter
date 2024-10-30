5e36108bfb8da1895fb566b445f01d2f26264e5c
siraajul <shirajulislamparvez@gmail.com>
2024-10-31 05:06:17 +0600

    MOD:-->Cupertino As a Default Page

--- lib/app.dart
+++ lib/app.dart
@@ -1,4 +1,6 @@
+import 'package:currency_converter/screens/home_page_cupertino.dart';
 import 'package:currency_converter/screens/home_page_material.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class MyApp extends StatelessWidget {
@@ -9,6 +11,18 @@ class MyApp extends StatelessWidget {
     return  MaterialApp(
         debugShowCheckedModeBanner: false,
         home: HomePage(),
+    );
+  }
+}
+
+class MyCupertinoApp extends StatelessWidget {
+  const MyCupertinoApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  CupertinoApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePageCupertino(),
     );
   }
 }
